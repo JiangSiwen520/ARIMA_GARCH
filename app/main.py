5baea5b8d9c53fa7741a6d2c1881a691d8d19319
JiangSiwen520 <115335073+JiangSiwen520@users.noreply.github.com>
2023-05-02 21:09:44 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
 from flask import Flask
 
 
-from test import route_test
+import pandas as pd
 app = Flask(__name__)     #实例化一个Flask对象
-app.register_blueprint(route_test,url_prefix='/test')
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, Welcome to CloudBase_JSW, 2023-02-13!!!->2023.5.1\n'
 @app.route('/new')
 def fun2():
-    #df = pd.read_excel('000001.xlsx')
+    try:
+        df = pd.read_excel('000001.xlsx')
+    except:
+        continue
     return 'Hello, Welcome to CloudBase_JSW, 2023-02-13!!!->2023.5.2\n'
 
 
