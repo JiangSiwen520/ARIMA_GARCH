8d83c0a5169c097a592d3837efa30f731f0711ac
JiangSiwen520 <115335073+JiangSiwen520@users.noreply.github.com>
2023-05-02 15:38:12 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from flask import Flask
+import model
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, Welcome to CloudBase_JSW, 2023-02-13!!!->2023.5.1\n'
-
+@app.route('/new')
+def fun2():
+    return 'Hello, Welcome to CloudBase_JSW, 2023-02-13!!!->2023.5.2\n'
 
 
 if __name__ == "__main__":
